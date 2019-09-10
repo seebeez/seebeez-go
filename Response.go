3857e749e7f390c987feb0bf0cf51a322516c684
Rafiul <rafiul2111@gmail.com>
2019-09-11 01:49:54 +0600

    Remove unnecessary error check

--- Response.go
+++ Response.go
@@ -3,7 +3,6 @@ package seebeez
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 )
 
@@ -13,10 +12,10 @@ type response struct {
 
 // Byte returns the []byte format of the response
 func (r *response) Byte() []byte {
-	body, err := ioutil.ReadAll(bytes.NewReader(r.Bytes))
-	if err != nil {
-		return []byte("")
-	}
+	body, _ := ioutil.ReadAll(bytes.NewReader(r.Bytes))
+	//if err != nil {
+	//	return []byte("")
+	//}
 	return body
 }
 
@@ -28,10 +27,6 @@ func (r *response) String() string {
 // ResInfo returns response containing dispatched job information
 func (r *response) Info() (ResInfo, error) {
 	var info ResInfo
-	fmt.Println(string(r.Byte()))
 	err := json.Unmarshal(r.Byte(), &info)
-	if err != nil {
-		return ResInfo{}, err
-	}
-	return info, nil
+	return info, err
 }
